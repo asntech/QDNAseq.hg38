2ee6a8f2af9a446eb819af6ae4b9f194e8d3b8d3
asntech <azez.khan@gmail.com>
2020-10-22 18:47:57 +0200

    initial commit

---
+++ R/QDNAseq.hg38.R
@@ -0,0 +1,190 @@
+#########################################################################/**
+# @RdocData hg38.1000kbp.SR50
+#
+# @title "Hg38 1000kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg38.500kbp.SR50
+#
+# @title "Hg38 500kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg38.100kbp.SR50
+#
+# @title "Hg38 100kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg38.50kbp.SR50
+#
+# @title "Hg38 50kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg38.30kbp.SR50
+#
+# @title "Hg38 30kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg38.15kbp.SR50
+#
+# @title "Hg38 15kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg38.10kbp.SR50
+#
+# @title "Hg38 10kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg38.5kbp.SR50
+#
+# @title "Hg38 5kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData hg38.1kbp.SR50
+#
+# @title "Hg38 1kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+# EOF
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# QDNAseq.hg38
+# QDNAseq.hg38: QDNAseq bin annotation for hg38
+
+[![HitCount](http://hits.dwyl.io/asntech/QDNAseq.hg38.svg)](http://hits.dwyl.io/asntech/QDNAseq.hg38)
+![R-CMD-check](https://github.com/asntech/QDNAseq.hg38/workflows/R-CMD-check/badge.svg)
+
+This package is a fork of Bioconductor R package
+[‘QDNAseq.hg19’](https://bioconductor.org/packages/release/bioc/html/QDNAseq.hg19.html)
+This package provides QDNAseq bin annotations of size `5, 10, 15, 30, 50, 100, 500 and 1000` kbp for the human genome build hg38
+
+## Installation
+
+Install `hg38` package from GitHub:
+
+``` r
+#Install the QDNAseq.hg38 package using remotes
+remotes::install_github("asntech/QDNAseq.hg38")
+#or devtools
+devtools::install_github("asntech/QDNAseq.hg38")
+```
+
+## Use QDNAseq.hg38
+
+``` r
+library(QDNAseq)
+library(QDNAseq.hg38)
+bins <- getBinAnnotations(binSize=50, genome="hg38")
+```
+
+Find more details about QDNAseq here: https://doi.org/doi:10.18129/B9.bioc.QDNAseq
